c7b367f412bece563274b6bd5ebf8d8978dd8445
Joachim <jotomaniak@gmail.com>
2023-03-22 00:49:58 +0100

    Menu updated to fit specifications

--- app/src/main/java/com/example/dzienniczekx3/MainActivity.kt
+++ app/src/main/java/com/example/dzienniczekx3/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.dzienniczekx3
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
 
@@ -16,13 +18,37 @@ class MainActivity : AppCompatActivity() {
 
         var loginButon = findViewById<Button>(R.id.loginButton)
         loginButon.setOnClickListener {
-            switchActivities()
+            switchActivities(login)
         }
     }
 
-    fun switchActivities(){
+    fun switchActivities(x: String){
         val newIntent = Intent(this, infoUzytkownik::class.java)
+        newIntent.putExtra("Username", x)
         startActivity(newIntent)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.mnu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(intent.getStringExtra("Username")!=null){
+            val id = item.itemId
+            if(id == R.id.firstItem){
+                val nowy = Intent(this,infoUzytkownik::class.java)
+                startActivity(nowy)
+                finish()
+            }
+            else if (id == R.id.secondItem){
+                val nowy = Intent(this,activity_oceny::class.java)
+                startActivity(nowy)
+                finish()
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 
 }
